12b90d721842e823a08e122581cdf5ab354614d1
Pierre TILAK <pierre.tilak@aeronavics.com>
2019-08-29 10:04:25 +1200

    Add openssl build and installl

---
+++ qgc_android/scripts/build_openssl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## See doc here : https://doc.qt.io/qt-5/android-openssl-support.html
+git clone https://github.com/openssl/openssl
+cd openssl
+git checkout OpenSSL_1_1_1
+export PATH="/opt/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin":$PATH
+export ANDROID_NDK='/opt/android-ndk'
+export ANDROID_SDK='/opt/android-sdk'
+./Configure shared android-arm  -D__ANDROID_API__=21 --prefix=/opt/android-ndk/platforms/android-21/arch-arm
+make CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" build_libs
+make install
