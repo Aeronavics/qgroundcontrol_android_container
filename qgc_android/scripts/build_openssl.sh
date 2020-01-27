ff2d5bb4b789d782b65385ea2d967bb6e5066140
James Morritt <james1morritt@gmail.com>
2020-01-28 12:07:29 +1300

    changed ndk path to be consistent with the new environment

--- qgc_android/scripts/build_openssl.sh
+++ qgc_android/scripts/build_openssl.sh
@@ -3,9 +3,9 @@
 git clone https://github.com/openssl/openssl
 cd openssl
 git checkout OpenSSL_1_1_1
-export PATH="/opt/android-ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin":$PATH
-export ANDROID_NDK='/opt/android-ndk'
+export PATH="/opt/android-sdk/ndk-r20b/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin":$PATH
+export ANDROID_NDK='/opt/android-sdk/ndk-r20b'
 export ANDROID_SDK='/opt/android-sdk'
-./Configure shared android-arm  -D__ANDROID_API__=21 --prefix=/opt/android-ndk/platforms/android-21/arch-arm
+./Configure shared android-arm  -D__ANDROID_API__=21 --prefix=/opt/android-sdk/ndk-r20b/platforms/android-21/arch-arm
 make CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" build_libs
 make install
